a8c418f967d2748315ba3278d575d0119b37785d
BrunoClemes <Brunojoseclemes@hotmail.com>
2024-02-20 15:55:17 -0300

    Desafio

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log ("OLá mundão")
+
+let mediaRankeadas = calculaRankeadas(200 ,5);
+
+function calculaRankeadas(victories, defeats ){
+    return victories - defeats; 
+}
+
+  
+    if ( mediaRankeadas < 10 ) {
+    Rankeadas = "Ferro"
+} else if (mediaRankeadas >= 11 && mediaRankeadas <= 20) {
+    Rankeadas = "Bronze"
+} else if (mediaRankeadas >= 21 && mediaRankeadas <=  50) {
+    Rankeadas = "Prata"
+}else if (mediaRankeadas >= 51 && mediaRankeadas <= 80) {
+    Rankeadas = "Ouro"
+}else if (mediaRankeadas >= 81 && mediaRankeadas <= 90) {
+    Rankeadas = "Diamante"
+}else if (mediaRankeadas >= 91 && mediaRankeadas <= 100) {
+    Rankeadas = " Lendário"
+}else if (mediaRankeadas >  101) {
+    Rankeadas = "Imortal"
+}
+
+console.log(`O Herói de saldo de vitórias ${mediaRankeadas} , está no nivel de ${Rankeadas} `);
